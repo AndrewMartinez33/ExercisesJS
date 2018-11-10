80f6ddcd046f16e7e8f93f0d3579009d3e62f502
Andrew A. Martinez <andrew.a.martinez33@gmail.com>
2018-11-10 10:52:36 -0800

    JS exercise for 11/10/18

---
+++ code-wars/exercise5.js
@@ -0,0 +1,15 @@
+// Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+// Examples
+// pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+// pigIt('Hello world !');     // elloHay orldway !
+
+function pigIt(str){
+  let array = str.split(" ");
+  for (let [arrIndex, value] of array.entries()) {
+    if (value.search(/[w]/g)) {
+      array[arrIndex] = `${value.substring(1)}${value[0]}${'ay'}`
+    } 
+  }
+  return array.join(' ');
+}
